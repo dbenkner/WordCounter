f9cc5c0f86e616f8c15e4f6b1319f6e00fbd1515
David Benkner <d.benkner@gmail.com>
2024-04-23 15:52:30 -0400

    Method to read files

---
+++ WordCounter/FileReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    public class FileReader
+    {
+         public static string ReadFile(string path)
+        {
+            string res;
+            try
+            {
+                var file = File.ReadAllBytes(path);
+                res = Encoding.UTF8.GetString(file);
+                return res;
+            } catch (Exception System) { 
+                Console.WriteLine("Invalid File!");   
+                Console.WriteLine(System);
+                res = "Exception";
+                return res;
+            }
+           
+        }
+    }
+}
